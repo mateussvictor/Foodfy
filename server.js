dc71389c2758f5a01afce3976df020b536fd3cdf
Mateus Victor <mateus.svictor@gmail.com>
2020-08-24 21:24:34 -0300

    feat: add method-override requirement

--- server.js
+++ server.js
@@ -1,31 +1,21 @@
 const express = require('express');
 const nunjucks = require('nunjucks');
-const recipes = require('./data.js');
+const methodOverride = require('method-override');
+const routes = require('./routes');
 
 const server = express();
 
+server.use(express.urlencoded({ extended: true }));
 server.use(express.static('public'));
+server.use(methodOverride('_method'));
+server.use(routes);
+
 server.set('view engine', 'njk');
 
 nunjucks.configure('views', {
   express: server,
   autoescape: false,
   noCache: true,
-});
-
-server.get('/', (req, res) => res.render('home', { recipes }));
-
-server.get('/about', (req, res) => res.render('about'));
-
-server.get('/recipes', (req, res) => res.render('recipes', { recipes }));
-
-server.get('/recipes/:id', (req, res) => {
-  const { id } = req.params;
-  if (!recipes[id]) {
-    return res.render('error');
-  }
-
-  return res.render('recipe', { item: recipes[id] });
 });
 
 server.listen(5000, () => {
